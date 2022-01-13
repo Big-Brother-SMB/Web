a4235e4cc13ff94a59ac3bd16597e54b39a0a2b3
Azerthias <matthias.hayart@gmail.com>
2022-01-13 21:15:29 +0100

    modif amis

--- public/confirmation/desinscription/desinscription.js
+++ public/confirmation/desinscription/desinscription.js
@@ -18,6 +18,9 @@ const jour = ["1lundi", "2mardi","3jeudi","4vendredi"];
 function path(j,h){
     return "foyer_midi/semaine"+ parseInt(sessionStorage.getItem("week")) + "/" + jour[j] + "/" + (11 + h) + "h"
 }
+function reload(){
+    window.location.reload(true)
+}
 
 let user = sessionStorage.getItem("user");
 let j = sessionStorage.getItem("j");
@@ -27,14 +30,38 @@ console.log(path(j,h))
 database.ref(path(j,h) + "/ouvert").once('value').then(function(snapshot) {
     if(snapshot.val() == 3 || snapshot.val() == 5){
         window.location.href = menu;
+    }else if(snapshot.val() == 7){
+        robi();
     }else{
+        document.getElementById("amis").style = "display:none"
+        document.getElementById("pAmis").style = "display:none"
         suite();
     }
 });
 
+function robi(){
+    console.log("robi")
+    let divAmis = document.getElementById("amis")
+    database.ref(path(j,h) + "/demandes/" + user + "/amis").once("value", function(snapshot) {
+        divAmis.innerHTML = ""
+        snapshot.forEach(function(child) {
+            let ami = document.createElement("button")
+            ami.innerHTML = child.key + " (supprimer)"
+            ami.addEventListener("click", function() {
+                console.log("remove")
+                database.ref(path(j,h) + "/demandes/" + user + "/amis/" + child.key).remove();
+                reload()
+            })
+            divAmis.appendChild(ami);
+        })
+        suite();
+    })
+}
+
+
 
 function suite(){
-    document.getElementById("info").innerHTML = "Vous êtes inscrit le X<br>Voulez vous vous desinscrire ?"
+    document.getElementById("info").innerHTML = "Vous êtes inscrit à cet horraire"
     document.getElementById("oui").addEventListener("click", function() {
         database.ref(path(j,h) + "/demandes/" + user).remove();
         window.location.href = menu;
