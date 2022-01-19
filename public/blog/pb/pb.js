fa4a3eb65e4060824c21860970f30744b2122bf5
Azerthias <matthias.hayart@gmail.com>
2022-01-19 21:51:58 +0100

    pb

--- public/blog/pb/pb.js
+++ public/blog/pb/pb.js
@@ -18,9 +18,10 @@ const menu = "../../menu/menu.html"
 
 document.getElementById("envoyer").addEventListener("click", function() {
     let message = document.getElementById("text Area").value
-    if(message.length != 0){
+    if(message.length > 200 && message.length < 20){
         console.log(message)
         database.ref("probleme/" + user).set(message)
+        document.getElementById("info").style.color = "black"
         document.getElementById("info").innerHTML = "envoi de votre message encours, veuillez patientez"
         setTimeout(function() {
             window.location.href = menu;
@@ -29,3 +30,14 @@ document.getElementById("envoyer").addEventListener("click", function() {
     }
     
 });
+
+
+document.getElementById("text Area").onkeydown =  function(){
+    let lettres = document.getElementById("text Area").value.length
+    document.getElementById("info").innerHTML = lettres + " lettres sur 200 authorisées (20 minimum)"
+    if(lettres > 200 || lettres < 20){
+        document.getElementById("info").style.color = "red"
+    }else{
+        document.getElementById("info").style.color = "black"
+    }
+};
