7445cac398948466f410fec6a3275264545ddc4e
Azerthias <64594536+Azerthias@users.noreply.github.com>
2022-01-24 08:45:31 +0100

    Update common.js

--- public/common.js
+++ public/common.js
@@ -44,7 +44,7 @@ let week = readIntCookie("week")
 
 // semaine
 
-let actualWeek = 3;
+let actualWeek = 4;
 const day = ["Lundi", "Mardi","Jeudi","Vendredi"];
 const dayWithMer = ["1lundi", "2mardi","err","3jeudi","4vendredi"]
 const dayNum = ["1lundi", "2mardi","3jeudi","4vendredi"];
