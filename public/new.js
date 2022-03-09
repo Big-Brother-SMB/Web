00c8c7f8fb6bf1d74b1268025bc99ef484e24fd3
Azerthias <matthias.hayart@gmail.com>
2022-03-09 18:59:30 +0100

    rangement

--- public/new.js
+++
@@ -1,6 +0,0 @@
-//https://developer.mozilla.org/fr/docs/Web/API/Notifications_API/Using_the_Notifications_API
-//https://mailtrap.io/blog/javascript-send-email/
-
-Notification.requestPermission().then(function(result) {
-    console.log(result);
-  });
