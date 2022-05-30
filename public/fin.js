04df11c5904be94993d8211293c9acaa23ac0f27
Azerthias <matthias.hayart@gmail.com>
2022-05-30 22:43:18 +0200

    Update fin.js

--- public/fin.js
+++ public/fin.js
@@ -1,33 +1,29 @@
-
 user = sessionStorage.getItem("user");
 email = sessionStorage.getItem("email");
 
+week = actualWeek
+
+writeCookie("user",user)
+writeCookie("email",email)
+writeCookie("week",week)
+writeCookie("RGPD",true)
+
+database.ref("users/" + user + "/email").set(email);
+database.ref("users/" + user + "/send mail").set(true)
 
 
-function suite(){
-    week = actualWeek
+database.ref("users/" + user + "/classe").once("value", function(snapshot) {
+    classe = snapshot.val()
+    if(classe != null){
+        
+        writeCookie("classe",classe)
+    }
+})
 
-    writeCookie("user",user)
-    writeCookie("email",email)
-    writeCookie("week",week)
-    writeCookie("RGPD",true)
-    
-    database.ref("users/" + user + "/email").set(email);
-    database.ref("users/" + user + "/send mail").set(true)
-    
-    
-    database.ref("users/" + user + "/classe").once("value", function(snapshot) {
-        classe = snapshot.val()
-        if(classe != null){
-            
-            writeCookie("classe",classe)
-        }
-    })
+setTimeout(function() {
+    window.location.href = "menu/menu.html";
+},2000);
 
-    setTimeout(function() {
-        window.location.href = "menu/menu.html";
-    },2000);
-}
 
 
 
