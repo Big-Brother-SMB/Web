1197c5ed326fca5fd0ca366021e2e6d1815828fa
donut-masque <donut.masque@gmail.com>
2022-08-14 09:38:53 +0200

    ouvre sur le tuto à la première connexion

--- public/fin.js
+++ public/fin.js
@@ -12,22 +12,23 @@ database.ref("users/" + user + "/email").set(email);
 database.ref("users/" + user + "/send mail").set(true)
 
 
-database.ref("users/" + user + "/classe").once("value", function(snapshot) {
-    classe = snapshot.val()
+setTimeout(function() {
+  database.ref("users/" + user).once("value", function(snapshot1) {
+    classe = snapshot1.child("classe").val()
     if(classe != null){
-        
         writeCookie("classe",classe)
     }
-})
-
-setTimeout(function() {
-    database.ref("modo/users/"+ user).once('value',function(snapshot){
-        if(snapshot.val()===1){
-          window.location.href = "/admin/menu/menu.html";
-        } else {
-          window.location.href = "/menu/menu.html";
-        }
+    database.ref("modo/users/"+ user).once('value',function(snapshot2){
+      if(snapshot2.val()===1){
+        window.location.href = "/admin/menu/menu.html";
+      } else if (snapshot1.child("tuto").val()!=true) {
+        window.location.href = "/tuto/tuto.html";
+        database.ref("users/" + user + "/tuto").set(true)
+      } else {
+        window.location.href = "/menu/menu.html";
+      }
     })
+  })
 },1000);
 
 
