e5d33ad63390483f0c42b7385e410eddf09fb67b
donut-masque <donut.masque@gmail.com>
2022-09-02 17:29:40 +0200

    list

--- List/listFusion.py
+++ List/listFusion.py
@@ -14,63 +14,82 @@ firebase_admin.initialize_app(cred, {
 
 rows=[]
 
-with open("Liste_2.csv") as file:
+with open("2.csv") as file:
     data_reader = csv.reader(file)
-    for line in data_reader:
+    for rowB in data_reader:
         row=[]
-        rowB=line[0].split(";")
         row.append(rowB[1])
+        row.append("")
+        row.append(rowB[0])
+        row.append(rowB[8])
+
         row.append(rowB[2])
         row.append(rowB[3])
-        row.append(rowB[10])
+
+        row.append("")
+        row.append("")
+        row.append("")
+
         row.append(rowB[4])
         row.append(rowB[5])
-        row.append("")
-        row.append("")
-        row.append("")
         row.append(rowB[6])
         row.append(rowB[7])
-        row.append(rowB[8])
-        row.append(rowB[9])
         rows.append(row)
-with open("Liste_1.csv") as file:
+with open("1.csv") as file:
     data_reader = csv.reader(file)
-    for line in data_reader:
+    for rowB in data_reader:
         row=[]
-        rowB=line[0].split(";")
         row.append(rowB[1])
-        row.append(rowB[2])
-        row.append(rowB[3])
-        row.append(rowB[11])
-        row.append(rowB[4])
-        row.append(rowB[5])
-        row.append(rowB[6])
-        row.append(rowB[7])
-        row.append(rowB[8])
-        row.append(rowB[9])
-        row.append(rowB[10])
-        rows.append(row)
-with open("Liste_T.csv") as file:
-    data_reader = csv.reader(file)
-    for line in data_reader:
-        row=[]
-        rowB=line[0].split(";")
-        row.append(rowB[1])
-        row.append(rowB[2])
-        row.append(rowB[3])
-        row.append(rowB[11])
-        row.append(rowB[4])
-        row.append(rowB[5])
-        row.append(rowB[6])
-        row.append(rowB[7])
         row.append("")
-        row.append(rowB[8])
+        row.append(rowB[0])
         row.append(rowB[9])
-        row.append(rowB[10])
+
+        row.append(rowB[2])
+        row.append(rowB[3])
+
+        row.append(rowB[4])
+        row.append(rowB[5])
+        row.append(rowB[6])
+
+        row.append(rowB[7])
+        row.append(rowB[8])
+        row.append("")
+        row.append("")
+        rows.append(row)
+with open("T.csv") as file:
+    data_reader = csv.reader(file)
+    for rowB in data_reader:
+        row=[]
+        row.append(rowB[1])
+        row.append("")
+        row.append(rowB[0])
+        row.append(rowB[9])
+
+        row.append(rowB[2])
+        row.append(rowB[3])
+
+        row.append(rowB[4])
+        row.append(rowB[5])
+        row.append("")
+
+        row.append(rowB[6])
+        row.append(rowB[7])
+        row.append(rowB[8])
+        row.append("")
         rows.append(row)
 
 
+for i in range(len(rows)):
+    if rows[i][0]!="":
+        l=rows[i][0].split(" ")
+        if len(l)==2:
+            rows[i][0]=l[0]
+            rows[i][1]=l[1]
+        else:
+            rows[i][1]="err"
+            print("err")
 with open("list.csv","w") as file:
 	dataW = csv.writer(file)
 	for i in rows:
-		dataW.writerow(i)
+            if i[0]!="":
+                dataW.writerow(i)
