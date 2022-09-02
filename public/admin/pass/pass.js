7e66744f75c855dbfe85f90d46a09d5de087e273
donut-masque <donut.masque@gmail.com>
2022-09-02 19:06:07 +0200

    update pass admin

--- public/admin/pass/pass.js
+++ public/admin/pass/pass.js
@@ -1,4 +1,5 @@
 let scanB = document.getElementById("scanB")
+let inscB = document.getElementById("inscB")
 let dayP = document.getElementById("day")
 
 
@@ -84,13 +85,12 @@ database.ref("users").once("value", function(snapshot){
         users_code.set(snapshot.child(child.key+"/code barre").val(),child.key)
     })
     autocomplete(inputName, utilisateurs,function(val){
+        
         searchName(val,false)
         inputCodeBar.value = snapshot.child(val+"/code barre/").val()
     });
 })
 function search(c,scan){
-    scanB.style.visibility="hidden";
-
     if(code == c){
         return;
     }
@@ -111,11 +111,12 @@ function search(c,scan){
 let h2=h
 function searchName(name,scan){
     h2=h
-    scanB.style.visibility="hidden";
 
     database.ref("foyer_midi/semaine" + actualWeek + "/" + j + h + "/inscrits/" + name).once("value", function(snapshot) {
         if(snapshot.val() != null){
             if(scan==true){
+                scanB.style.visibility="hidden";
+                inscB.style.visibility="hidden";
                 if(snapshot.child('scan').val()==null){
                     NBscan++
                 }
@@ -123,6 +124,7 @@ function searchName(name,scan){
                 affichagePassages()
             }else{
                 scanB.style.visibility="visible";
+                inscB.style.visibility="hidden";
             }
             document.getElementById("pass").innerHTML = "<img width=\"200\" height=\"200\" alt=\"\" src=\"../../Images/ok.png\" />"
         }else{
@@ -133,13 +135,16 @@ function searchName(name,scan){
             }
             database.ref("foyer_midi/semaine" + actualWeek + "/" + j + h2 + "/inscrits/" + name).once("value", function(snapshot) {
                 if(snapshot.val() != null){
+                    scanB.style.visibility="visible";
+                    inscB.style.visibility="hidden";
                     if(h2=="/11h"){
                         document.getElementById("pass").innerHTML = "<img width=\"200\" height=\"200\" alt=\"\" src=\"../../Images/ok11.png\" />"
                     }else{
                         document.getElementById("pass").innerHTML = "<img width=\"200\" height=\"200\" alt=\"\" src=\"../../Images/ok12.png\" />"
                     }
-                    scanB.style.visibility="visible";
                 }else{
+                    scanB.style.visibility="hidden";
+                    inscB.style.visibility="visible";
                     document.getElementById("pass").innerHTML = "<img width=\"200\" height=\"200\" alt=\"\" src=\"../../Images/croix.png\" />"
                 }
             })
@@ -149,6 +154,7 @@ function searchName(name,scan){
 
 scanB.addEventListener("click",function(){
     scanB.style.visibility="hidden";
+    inscB.style.visibility="hidden";
     database.ref("foyer_midi/semaine" + actualWeek + "/" + j + h2 + "/inscrits/" + document.getElementById("name").value).once("value", function(snapshot) {
         if(snapshot.val() != null){
             if(snapshot.child('scan').val()==null && h2==h){
@@ -157,6 +163,24 @@ scanB.addEventListener("click",function(){
             database.ref("foyer_midi/semaine" + actualWeek + "/" + j + h2 + "/inscrits/" + document.getElementById("name").value + "/scan").set(hash())
             affichagePassages()
         }
+        h2=h
+    })
+})
+
+inscB.addEventListener("click",function(){
+    scanB.style.visibility="hidden";
+    inscB.style.visibility="hidden";
+    database.ref("foyer_midi/semaine" + actualWeek + "/" + j + h + "/inscrits/" + document.getElementById("name").value).once("value", function(snapshot) {
+        if(snapshot.val()==null){
+            NBinscrit++
+        }
+        if(snapshot.child('scan').val()==null){
+            NBscan++
+        }
+        database.ref("foyer_midi/semaine" + actualWeek + "/" + j + h + "/inscrits/" + document.getElementById("name").value + "/user").set(0)
+        database.ref("foyer_midi/semaine" + actualWeek + "/" + j + h + "/inscrits/" + document.getElementById("name").value + "/scan").set(hash())
+        document.getElementById("pass").innerHTML = "<img width=\"200\" height=\"200\" alt=\"\" src=\"../../Images/ok.png\" />"
+        affichagePassages()
         h2=h
     })
 })
