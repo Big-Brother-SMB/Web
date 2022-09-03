0a406d2c2c298c391b17f4b2591450e30c71353a
donut-masque <donut.masque@gmail.com>
2022-09-03 15:53:32 +0200

    bug pop up

--- public/menu/menu.js
+++ public/menu/menu.js
@@ -496,3 +496,29 @@ function handleTouchMove(evt) {
 document.getElementById("nav hide").addEventListener("click",function(){
     document.querySelectorAll("nav")[0].style.visibility = "hidden"
 })*/
+
+
+
+//-------------------------------Pop-Up---------------------------------------------
+
+const overlay = document.getElementById('overlay')
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+database.ref("users/" + user + "/tuto").once("value", function(snapshot) {
+    tuto = snapshot.val()
+    if(tuto != true){
+      const modal = document.getElementById('modal')
+      openModal(modal)
+    }
+})
