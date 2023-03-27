f49ca15a65913354fc05916b8c3447412733768d
donut-masque <donut.masque@gmail.com>
2023-03-27 20:10:18 +0200

    reset score 0

--- List/test.py
+++ List/test.py
@@ -6,18 +6,11 @@ from firebase_admin import db
 cred = credentials.Certificate('key.json')
 
 firebase_admin.initialize_app(cred, {
-  'databaseURL': "https://big-brother-ac39c-default-rtdb.europe-west1.firebasedatabase.app/"
+  'databaseURL': "https://smb-384c5-default-rtdb.europe-west1.firebasedatabase.app"
 })
 
 x=db.reference("users").get()
-tour=0
 for n in x:
-    o=x.get(n).get("score")
-    score=0
-    for s in o:
-      score+=o.get(s).get("value")
-    if score<0:
-      tour+=1
-      print(n)
-print(tour)
+  print(n)
+  db.reference("users/" + n + "/score").delete()
     
