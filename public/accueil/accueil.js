9f2727150e895d77ce7686ba79ab4717e905fc53
donut-masque <donut.masque@gmail.com>
2023-07-04 10:55:20 +0200

    style page d'accuille + couleur

--- public/accueil/accueil.js
+++ public/accueil/accueil.js
@@ -1,16 +1,29 @@
 let side = document.getElementById("mySidenav")
-side.style.width="0px"
-let out_side_function = function(e){
-    if (!side.contains(e.target)){
-        side.style.width = "0px"
-        window.removeEventListener('click',out_side_function)
-    }
-}
-document.getElementById("logo").addEventListener("click",function() {
-    if(side.style.width=="0px"){
+side.addEventListener("mouseenter",function() {
+    if(window.innerWidth>=1400){
         side.style.width = "250px";
-        setTimeout(function() {
-            window.addEventListener('click',out_side_function);
-        }, 50);
     }
 });
+
+side.addEventListener("mouseleave",function() {
+    if(window.innerWidth>=1400){
+        side.style.width = "80px";   
+    }
+});
+
+let btn_menu = document.getElementById("btn_menu")
+btn_menu.addEventListener("click",function() {
+    if(window.innerWidth<1400){
+        if(side.style.height=="0px" || side.style.height==""){
+            side.style.height = "calc(100vh - 8em - 33px)";
+            side.style.width = "100%";
+        }else{
+            side.style.height = "0";
+            side.style.width = "100%";
+        }
+    }
+});
+
+/*side.addEventListener("mouseleave",function() {
+    side.style.width = "80px";
+});*/
