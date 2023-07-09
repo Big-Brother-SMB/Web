8bff326c4f6a1bbf9c03f88875bbacb4297e8a23
donut-masque <donut.masque@gmail.com>
2023-07-09 18:30:25 +0200

    csv2sql

--- List/convertion_firebase_vers_js.py
+++ List/convertion_firebase_vers_js.py
@@ -26,14 +26,11 @@ with open("list.csv") as file:
     for rowB in data_reader:
         cur.execute("INSERT INTO users(email,uuid,first_name,last_name,classe,code_barre,admin) VALUES('" + rowB[13] + "','" + str(uuid.uuid4()) + "','" + rowB[1] + "','" + rowB[0] + "','" + rowB[2] + "','" + rowB[3] + "',0)")
 """
-
-
 racine=db.reference("/").get()
 list_user=[]
 for u in racine.get("users"):
     user={}
-    user['id']=str(uuid.uuid4())
-    user['idU']=u
+    user['idUser']=u
     t = u.split('@')[0].split('µ')
     user['prenom']=t[0]
     user['nom']=""
@@ -55,71 +52,21 @@ for u in racine.get("users"):
         for prio in prios:
             user['priorites'].append(prio)
     list_user.append(user)
-    
-    #score=u.get("score")
-    #tuto=u.get("tuto")
 
-
-
-
-rFoyer=db.reference("/foyer_midi").get()
-for s in racine.get("foyer_midi"):
-    sW = racine.get("foyer_midi").get(s)
-    for j in sW:
-        if j=="1lundi" or j=="2mardi" or j=="3jeudi" or j=="4vendredi":
-            jW = sW.get(j)
-            for h in jW:
-                hW = jW.get(h)
-                
-                cout=hW.get("cout")
-                gp=hW.get("gratuit prioritaires")
-                ouv=hW.get("ouvert")
-                places=hW.get("places")
-
-                prios=hW.get("prioritaires")
-                if prios!=None:
-                    for prio in prios:
-                        pass
-                inss=hW.get("inscrits")
-                if inss!=None:
-                    for ins in inss:
-                        idU=ins
-                        id="a"
-                        for x in list_user:
-                            if x['idU']==idU:
-                                id=x["id"]
-                        #print(ins)
-                        ins=inss.get(ins)
-                        scan=ins.get("scan")
-                        amis = ins.get('amis')
-                        if amis!=None:
-                            for ami in amis:
-                                idUA=ami
-                                idA="a"
-                                for x in list_user:
-                                    if x['idU']==idUA:
-                                        idA=x["id"]
-                                        rFoyer[s][j][h]['inscrits'][idU]['amis'][idA] = rFoyer[s][j][h]['inscrits'][idU]['amis'][idUA]
-                                        del rFoyer[s][j][h]['inscrits'][idU]['amis'][idUA]
-                                #print(ami)
-                                pass
-                        rFoyer[s][j][h]['inscrits'][id] = rFoyer[s][j][h]['inscrits'][idU]
-                        del rFoyer[s][j][h]['inscrits'][idU]
-        elif j=="menu": 
-            menu = sW.get("menu")
-
-
-list_user
-rFoyer
+with open('list.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for u in list_user:
+        writer.writerow([str(u['classe']),str(u['prenom']),str(u['nom']),str(u['code barre']),str(u['email'])])
 
 
 #print(rFoyer)
 #pret/users====histPoint/priorites
+"""
 for u in list_user:
     if u['prenom']!='None':
         print(u)
         cur.execute("INSERT INTO users(email,uuid,first_name,last_name,classe,code_barre,admin) VALUES('" + str(u['email']) + "','" + str(u['id']) + "','" + str(u['prenom']) + "','" + str(u['nom']) + "','" + str(u['classe']) + "','" + str(u['code barre']) + "',0)")
-con.commit()
+con.commit()"""
 #uuid UUID, first_name text, last_name text, email text, code_barre CHAR[5], classe text, tuto boolean,admin int2
 #res = cur.execute("SELECT name FROM sqlite_master")
 #res.fetchone()
---
+++ List/csv2sql.py
@@ -0,0 +1,18 @@
+import csv
+from email import message
+from operator import index, indexOf
+from datetime import datetime
+import time
+
+import sqlite3
+import uuid
+con = sqlite3.connect("../../main.db")
+cur = con.cursor()
+
+
+with open("list.csv") as file:
+    data_reader = csv.reader(file)
+    for rowB in data_reader:
+        cur.execute("INSERT INTO users(email,uuid,first_name,last_name,classe,code_barre,admin) VALUES('" + rowB[4] + "','" + str(uuid.uuid4()) + "','" + rowB[1] + "','" + rowB[2] + "','" + rowB[0] + "','" + rowB[3] + "',1)")
+
+con.commit()
