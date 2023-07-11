1419c90214a752ac89188ddf1d5c3826ce4b4b83
donut-masque <donut.masque@gmail.com>
2023-07-11 11:49:04 +0200

    test

--- server/User.js
+++ server/User.js
@@ -72,8 +72,12 @@ module.exports = class User{
     static searchToken(token){
         return new Promise(function(resolve, reject) {
           try{
+          db.get("SELECT * FROM token",[], (err, data) => {
+            console.log(data)
+          })
           db.get("SELECT uuid FROM token where token=?",[token], (err, data) => {
             try {
+              console.log(token,data)
               if(data!=undefined){
                 db.run("UPDATE token SET last_use=? where token=?",[hashHour(),token])
                 resolve(new User(data.uuid))
