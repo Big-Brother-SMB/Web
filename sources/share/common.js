d1454c9f7ee846e7506f5bd73130d29d55de308c
donut-masque <donut.masque@gmail.com>
2023-07-19 11:49:20 +0200

    bug

--- sources/share/common.js
+++ sources/share/common.js
@@ -132,6 +132,15 @@ export class common{
 
 
   static async startUp(){
+    //-----------------------screen size------------------------------------
+
+    const appHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+    window.addEventListener('resize', appHeight)
+    appHeight()
+
     //---------------------------cookie---------------------------
     let tablecookie = document.cookie.split('; ');
     this.cookie = {};
