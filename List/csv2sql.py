caa3d45573fa187b231798754e1f77b197aedb84
donut-masque <donut.masque@gmail.com>
2023-09-02 16:26:12 +0200

    list

--- List/csv2sql.py
+++ List/csv2sql.py
@@ -35,7 +35,27 @@ with open('list.csv') as file:
                     cur.execute('INSERT INTO user_groups(uuid,group2) VALUES("' + uuid_ + '","' + rowB[i] + '")')
 
 cur.execute('DELETE FROM group_list')
+
+list_prio+=['Club info','Matches Heads']
 for prio in list_prio:
     cur.execute('INSERT INTO group_list(group2) VALUES("' + prio + '")')
+
+
+cur.execute('DELETE FROM classe_list')
+
+t0 =["2A","2B","2C","2D","2E","2F","2G","2H","2I","2J","2K","2L"]
+t1 =["1A","1B","1C","1D","1E","1F","1G","1H","1I","1J","1K"]
+t2 =["TA","TB","TC","TD","TE","TF","TG","TH","TI","TJ","TK"]
+t3=["PCSI","PC","professeurs-personnel"]
+
+for e in t0:
+    cur.execute('INSERT INTO classe_list(classe,niveau) VALUES ("' + e + '",0)')
+for e in t1:
+    cur.execute('INSERT INTO classe_list(classe,niveau) VALUES ("' + e + '",1)')
+for e in t2:
+    cur.execute('INSERT INTO classe_list(classe,niveau) VALUES ("' + e + '",2)')
+for e in t3:
+    cur.execute('INSERT INTO classe_list(classe,niveau) VALUES ("' + e + '",3)')
+
 
 con.commit()
