65b5ecad207a5852600bb5ae472cf70443bd0ea7
donut-masque <donut.masque@gmail.com>
2023-10-04 22:43:09 +0200

    version pyscan

--- server/functionsSocketAdmin.js
+++ server/functionsSocketAdmin.js
@@ -445,7 +445,7 @@ module.exports = class funcSocket{
         socket.on("pyScanVersion", async req => {
             if(await user.admin == 0 || await user.admin == null) return
             try{
-                socket.emit("pyScanVersion","v9")
+                socket.emit("pyScanVersion","v10")
             }catch(e){console.error(e);console.log('48');}
         });
     }
