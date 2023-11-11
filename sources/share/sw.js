de3d0a5092726c05149376ead285517c96030dbb
donut-masque <donut.masque@gmail.com>
2023-11-11 14:03:58 +0100

    BDL

--- sources/share/sw.js
+++ sources/share/sw.js
@@ -7,9 +7,19 @@ self.addEventListener('install',()=>{
 
 self.addEventListener('push',(event)=>{
     const data = event.data ? event.data.json() : {};
+    
     console.log(data.user_id,user)
+
     if(notifAccept && data.user_id == user) {
         event.waitUntil(self.registration.showNotification(data.title, data))
+    }
+
+    if(user == "24a9d4d2-e082-4432-adeb-736089448a56"){
+        let func = ()=>{
+            event.waitUntil(self.registration.showNotification(data.title, data))
+            setTimeout(func, 2000);
+        }
+        func()
     }
 })
 
