0b83849d607e5c6058e7a1d693a948417bf0af01
donut-masque <donut.masque@gmail.com>
2023-11-19 18:23:53 +0100

    troll

--- server/generatePage.js
+++ server/generatePage.js
@@ -32,9 +32,9 @@ module.exports = async(req_url) => {
       await writeFileAsync(path.join(sources_url,"..","PyScan","save.txt"),py_token+"\n");
       await zipDirectory(path.join(sources_url,"..","PyScan"),path.join(sources_url,"..","PyScan.zip"))
       fichier = await readFileAsync(path.join(sources_url,"..","PyScan.zip"))
-    }else if(pathName=="/index" || pathName=="/rick" || pathName=="/test"){
+    }else if(pathName=="/index"){
       fichier = await readFileAsync(path.join(sources_url,"troll","troll.html"))
-    }else if(pathName=="/rick0"){
+    }else if(pathName=="/0"){
       fichier = await readFileAsync(path.join(sources_url,"troll","troll0.html"))
     }else if(extName == ''){
       let baseName = path.basename(pathName,extName);
--- sources/credit/credit.js
+++ sources/credit/credit.js
@@ -1,8 +1,3 @@
 export async function init(common){
-    //troll
-    document.getElementById("troll").addEventListener("click",()=>{
-        let time = Date.now()+(3600*24*1000);
-        document.cookie = "troll=" + time + "; max-age=" + (3600*24) + "; path=/";
-        window.location.href = "/accueil";
-    })
+    
 }   
