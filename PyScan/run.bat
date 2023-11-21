1956ec1bcf462577fac09559b976b661dbc2e152
donut-masque <donut.masque@gmail.com>
2023-11-21 19:36:00 +0100

    bug

--- PyScan/run.bat
+++ PyScan/run.bat
@@ -1,5 +1,4 @@
 pip install "python-socketio[client]"
 pip install pynput
 pip install playsound
-Set WshShell = WScript.CreateObject("WScript.Shell")
-WshShell.Run("python3 pyscan.py") 
+python3 pyscan.py
