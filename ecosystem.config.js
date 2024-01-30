86cb48fb3a32f7e3d977523cc51f528993712b0b
donut-masque <donut.masque@gmail.com>
2024-01-30 19:15:41 +0100

    git

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       user : 'john',
       host : '91.121.41.208',
       ref  : 'origin/main',
-      repo : 'https://github.com/Big-Brother-SMB/Web.git',
+      repo : 'git@github.com:Big-Brother-SMB/Web.git',
       path : '/home/site',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
