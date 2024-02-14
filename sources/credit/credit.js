85d70dd1289b1838a349fbf3a84169ec8d006133
donut-masque <donut.masque@gmail.com>
2024-02-14 19:09:34 +0100

    nouvelle function easter egg

--- sources/credit/credit.js
+++ sources/credit/credit.js
@@ -1,3 +1,15 @@
 export async function init(common){
-    
+    document.getElementById("zelda").addEventListener("click",async ()=>{
+        await common.socketAsync("setAchievement",{event:"zelda",value:1})
+        common.writeCookie("theme mode",6)
+        setTimeout(() => {
+          window.location.href = "/accueil";
+        }, 1000);
+    })
+
+    document.getElementById("troll").addEventListener("click",async ()=>{
+        let time = 3600*1;
+        document.cookie = "troll=" + (Date.now()+(time*1000)) + "; max-age=" + time + "; path=/";
+        window.location.href = "/accueil";
+    })
 }   
