f77be0f6409fdc6f44993ec446168303f6ff45bc
Aillax <axelstassen12@gmail.com>
2024-05-08 12:49:44 +0200

    NDC en cours

--- sources/NDC/NDC.js
+++ sources/NDC/NDC.js
@@ -1,22 +1,10 @@
 export async function init(common){
+    document.getElementById("jeuSelect").addEventListener("change",function(e){
+        document.getElementById("jeu").href="NDC/launcher.html?jeu="+document.getElementById("jeuSelect").value
 
-var images = {
-    "1": "images_jeu/exemple.jpg",
-    "2": "",
-    "3": ""
-  };
-  
-  function changeImage() {
-    var select = document.getElementById("jeuSelect");
-    var displayImage = document.getElementById("displayImage");
-    
-    if (select.value) {
-      displayImage.src = images[select.value];
-      displayImage.style.display = "block";
 
-      localStorage.setItem("selectedImageId", select.value);
-    } else {
-      displayImage.style.display = "none";
-    }
-  }
+    })
+
+
+
 }
