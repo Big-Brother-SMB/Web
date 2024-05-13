7fdfb3d18457ef484d9b6a52284383a5a325657c
donut-masque <donut.masque@gmail.com>
2024-05-13 19:49:05 +0200

    correctif

--- server/functionsSocketAdmin.js
+++ server/functionsSocketAdmin.js
@@ -457,7 +457,7 @@ module.exports = class funcSocket{
         socket.on("pyScanVersion", async req => {
             if(await user.admin == 0 || await user.admin == null) return
             try{
-                socket.emit("pyScanVersion","v34")
+                socket.emit("pyScanVersion","v35")
             }catch(e){console.error("fsA43");}
         });
     }
