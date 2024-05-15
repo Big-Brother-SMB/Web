de078005022ef40d9fea1869132b467c5e24ffdf
Aillax <axelstassen12@gmail.com>
2024-05-15 19:45:53 +0200

    NDC fonctionnalités

--- sources/NDC/NDC.js
+++ sources/NDC/NDC.js
@@ -1,10 +1,15 @@
 export async function init(common){
-    document.getElementById("jeuSelect").addEventListener("change",function(e){
-        document.getElementById("jeu").href="NDC/launcher.html?jeu="+document.getElementById("jeuSelect").value
-
-
-    })
-
-
-
+    var dervaleur; 
+    document.getElementById("jeuSelect").addEventListener("change", function(a) {
+        if(typeof dervaleur == "undefined"){ 
+            document.getElementById(document.getElementById("jeuSelect").value).classList.remove("cache");
+            dervaleur = document.getElementById("jeuSelect").value; 
+        }
+        else{
+            document.getElementById(dervaleur).classList.add("cache");
+            document.getElementById(document.getElementById("jeuSelect").value).classList.remove("cache");
+            dervaleur = document.getElementById("jeuSelect").value; 
+        }
+        document.getElementById("jeu").href="NDC/launcher.html?jeu="+document.getElementById("jeuSelect").value+".pyxapp"
+    });
 }
