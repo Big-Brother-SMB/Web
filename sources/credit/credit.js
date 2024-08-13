180633d9874e4134a21b2372e76d421752daaa8f
Aillax <axelstassen12@gmail.com>
2024-08-13 14:50:09 +0200

    maj credit

--- sources/credit/credit.js
+++ sources/credit/credit.js
@@ -1,9 +1,9 @@
-export async function init(common){
-    document.getElementById("zelda").addEventListener("click",async ()=>{
-        await common.socketAsync("setAchievement",{event:"zelda",value:1})
-        common.writeCookie("theme mode",6)
-        setTimeout(() => {
-          window.location.href = "/accueil";
-        }, 1000);
-    })
-}   
+//export async function init(common){
+    //document.getElementById("zelda").addEventListener("click",async ()=>{
+        //await common.socketAsync("setAchievement",{event:"zelda",value:1})
+        //common.writeCookie("theme mode",6)
+        //setTimeout(() => {
+          //window.location.href = "/accueil";
+        //}, 1000);
+    //})
+//}   
